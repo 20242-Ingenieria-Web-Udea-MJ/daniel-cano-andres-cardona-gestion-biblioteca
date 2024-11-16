06329f1e6ae58b05168cfa5dd2a9023d6a15bd6a
danielcanoh22 <daniel.canoh@udea.edu.co>
2024-11-16 16:34:55 -0500

    init config: supabase, auth0 :rocket:

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+import safeJsonStringify from "safe-json-stringify";
+import { useSession, signIn } from "next-auth/react";
+import axios from "axios";
+
+export async function getServerSideProps() {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
+  return {
+    props: {
+      users: safeJsonStringify(users),
+    },
+  };
+}
+
+export default function Home({ users }: any) {
+  const { data: session } = useSession();
+  if (!session) {
+    signIn("auth0");
+  }
+  console.log("session", session, users);
+  // const getProducts = async () => {
+  //   await axios.get("https://fakestoreapi.com/products").then((res: any) => {
+  //     console.log("res", res);
+  //   });
+  // };
+  // getProducts();
+
+  return (
+    <div
+      className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    ></div>
+  );
+}
